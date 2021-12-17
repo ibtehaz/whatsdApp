2c7837392e3579d0d67ae6dbda6459009269702f
ibtehaz <ibtehaz.utsay@gmail.com>
2021-12-16 22:07:24 -0800

    fix image bug

--- components/Login.js
+++ components/Login.js
@@ -5,14 +5,13 @@ function Login() {
     const { authenticate } = useMoralis();
     return (
         <div className="bg-black relative text-white">
-            <h1>I am login</h1>
 
             <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4"> 
-                <Image src="/../public/concept.png" alt="login" width={200} height={200} className="object-cover rounded-full animate-spin" />
+                <Image src="/concept.png" alt="login" width={200} height={200} className="object-cover rounded-full animate-spin" />
                 <button onClick={authenticate} className="bg-sky-800 rounded-lg p-5 font-bold animate-pulse">Login to the metaverse</button>
             </div>
             <div className="w-full h-screen">
-                <Image src="/../public/bg-hero.jpeg" layout="fill" objectFit="cover" />
+                <Image src="/bg-hero.jpeg" layout="fill" objectFit="cover" />
             </div>
         </div>
     )
